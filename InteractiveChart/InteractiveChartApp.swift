78b5139d3be6da7bea84f61f5b2f37df235b8320
jldavid <jldavid.mail@gmail.com>
2025-02-09 23:30:05 -0500

    Initial commit

---
+++ InteractiveChart/InteractiveChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveChartApp.swift
+//  InteractiveChart
+//
+//  Created by JL David on 2025-02-05.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
